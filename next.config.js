1820614bfd2217c3b2afafb1ebb5159de35ddceb
nGincode <fembinurilham@gmail.com>
2023-10-03 13:24:25 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["my.ecosystem.id"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
   },
 };
 
