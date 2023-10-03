5bc824569c6c81bec943f8f076252073e710ca54
nGincode <fembinurilham@gmail.com>
2023-10-03 11:57:20 +0700

    fix

--- server/controller/company.js
+++ server/controller/company.js
@@ -84,7 +84,7 @@ const putId = async (req, res) => {
     npwp: req.body.npwp,
     name: name,
     phone: phone,
-    email: email,
+    email: email ?? null,
     address: {
       jalan: jalan,
       block: block,
