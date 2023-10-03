c7b47c1bb055a578f929440827fe0918bfe78180
nGincode <fembinurilham@gmail.com>
2023-10-03 13:10:11 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["my.ecosystem.id"],
+  },
+};
 
 module.exports = nextConfig;
