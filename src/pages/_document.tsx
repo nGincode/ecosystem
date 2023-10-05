b2af7bf09e155849bdc6b9c2a86e79218602049d
nGincode <fembinurilham@gmail.com>
2023-10-05 11:47:38 +0700

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 {/* <link rel="icon" href="/img/logo.png" /> */}
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+                {/* <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" /> */}
                 <link rel="stylesheet" type="text/css" href="/app-assets/css/plugin/swiper-bundle.min.css" />
                 <link rel="stylesheet" type="text/css" href="/app-assets/icons/iconly/index.min.css" />
                 <link rel="stylesheet" type="text/css" href="/app-assets/icons/remix-icon/index.min.css" />
