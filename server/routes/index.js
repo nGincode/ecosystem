d188fddce3191c00dd50e1446cdb3d30015ccb91
fembi nur ilham <fembinurilham@gmail.com>
2024-02-27 10:11:00 +0700

    fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -35,6 +35,35 @@ router.get("/token", (req, res) => {
   res.send(jwt.sign({ data }, "fembinurilham"));
 });
 
+router.get("/cekdb", (req, res) => {
+  const dotenv = require("dotenv").config();
+  const config = {
+    database: dotenv.parsed.DATABASE,
+    username: dotenv.parsed.DATABASE_USERNAME,
+    password: dotenv.parsed.DATABASE_PASSWORD,
+    host: dotenv.parsed.HOST,
+    dialect: dotenv.parsed.DATABASE_DIALECT,
+  };
+  const Sequelize = require("sequelize");
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Connection has been established successfully.");
+    })
+    .catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    });
+
+  const data = { id: 1 };
+  res.send(jwt.sign({ data }, "fembinurilham"));
+});
+
 router.use("/user", verifToken, user);
 router.use("/efaktur", verifToken, efaktur);
 router.use("/permission", verifToken, permission);
